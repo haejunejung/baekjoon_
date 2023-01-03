33b7125693ae052c950886b287fa6ad159968b9b
haejunejung <jhj9422@unist.ac.kr>
2023-01-03 10:21:26 +0900

    Create main.cpp

---
+++ +2000번대/2630 색종이 만들기/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+int board[129][129];
+int N, blue, white;
+
+bool check(int fx, int fy, int lx, int ly)
+{
+    bool flag = board[fx][fy];
+    for (int sy = fy; sy <= ly; ++sy)
+    {
+        for (int sx = fx; sx <= lx; ++sx)
+        {
+            if (flag != board[sx][sy])
+                return false;
+        }
+    }
+    return true;
+}
+
+void partition(int fx, int fy, int lx, int ly)
+{
+    if ((fx < lx) && (fy < ly))
+    {
+        if (check(fx, fy, lx, ly))
+        {
+            if (board[fx][fy] == 1)
+                blue++;
+            else
+                white++;
+
+            return;
+        }
+        int mx = (fx + lx) / 2;
+        int my = (fy + ly) / 2;
+
+        partition(fx, fy, mx, my);
+        partition(mx + 1, fy, lx, my);
+        partition(fx, my + 1, mx, ly);
+        partition(mx + 1, my + 1, lx, ly);
+    }
+
+    if (check(fx, fy, lx, ly))
+    {
+        if (board[fx][fy] == 1)
+            blue++;
+        else
+            white++;
+
+        return;
+    }
+}
+
+int main()
+{
+    using namespace std;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    blue = 0;
+    white = 0;
+
+    cin >> N;
+    for (size_t i = 1; i <= N; ++i)
+    {
+        for (size_t j = 1; j <= N; ++j)
+            cin >> board[i][j];
+    }
+
+    partition(1, 1, N, N);
+
+    cout << white << "\n"
+         << blue;
+    return 0;
+}
